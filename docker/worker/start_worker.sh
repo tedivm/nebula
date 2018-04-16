4fc99851f0c9ebf06c2a8a2975052312b04704d9
Robert Hafner <tedivm@tedivm.com>
2018-04-15 18:36:34 -0700

    Delete beat schedule before restarting beat

--- docker/worker/start_worker.sh
+++ docker/worker/start_worker.sh
@@ -2,7 +2,10 @@
 
 if [ "$DISABLE_BEAT" = "true" ]
 then
+  echo 'Launching celery worker without beat'
   celery -A nebula.celery worker --loglevel=info --autoscale=10,2
 else
+  echo 'Launching celery worker with beat enabled'
+  rm -f /home/nebula/celerybeat-schedule
   celery -A nebula.celery worker --loglevel=info --autoscale=10,2 -B -s /home/nebula/celerybeat-schedule
 fi
