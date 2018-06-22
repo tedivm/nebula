d7db7903809790e04b82efe474b6eedf11efaf7e
Robert Hafner <tedivm@tedivm.com>
2018-06-21 22:27:33 -0700

    Fix variable name

--- docker/app/get_database.py
+++ docker/app/get_database.py
@@ -21,7 +21,7 @@ if 'AWS_SECRETS_SETTINGS' in os.environ:
 
     # Decrypted secret using the associated KMS CMK
     # Depending on whether the secret was a string or binary, one of these fields will be populated
-    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    get_secret_value_response = client.get_secret_value(SecretId=aws_secret_name)
     if 'SecretString' in get_secret_value_response:
         secret = get_secret_value_response['SecretString']
     else:
