95f24ecf26e0e33628031c87a370e23aa4d1513d
Robert Hafner <tedivm@tedivm.com>
2020-09-22 16:41:35 -0700

    Add ability to skip initial database connection check

--- docker/app/prestart.sh
+++ docker/app/prestart.sh
@@ -6,12 +6,16 @@ if [ -d /home/nebula/.aws ]; then
   chown -R root:root /root/.aws
 fi
 
-# Let the DB start
-database=`/app/get_database.py`
-echo "Waiting for postgres to be available at host '${database}'"
-while ! pg_isready -h ${database} > /dev/null 2> /dev/null; do
-  sleep 1
-done
+
+if [ -z "$SKIP_WAIT_FOR_PASSWORD" ]; then
+  # Let the DB start
+  database=`/app/get_database.py`
+  echo "Waiting for postgres to be available at host '${database}'"
+  while ! pg_isready -h ${database} > /dev/null 2> /dev/null; do
+    sleep 1
+  done
+fi
+
 
 echo 'Performing any database migrations.'
 python /app/db/manage.py version_control
